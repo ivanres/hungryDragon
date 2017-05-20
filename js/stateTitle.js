c2a0c6397842cd0da982ffdcc16387820806bc05
Ivan Rodriguez <iragination@gmail.com>
2017-05-19 21:24:05 -0500

    Adding Animation to the start screen

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -3,12 +3,21 @@ var StateTitle={
    preload:function()
     {
        game.load.spritesheet("buttons", "images/ui/buttons.png", 265, 75);
+       game.load.spritesheet("dragon", "images/main/dragon.png", 120, 85, 4);
     },
     
     create:function()
     {
         this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, "buttons", this.startGame, this, 6, 7, 6);
         this.buttonStart.anchor.set(0.5, 0.5);
+
+        //dragon
+    	this.dragon=game.add.sprite(game.world.centerX, game.world.centerY, "dragon");
+    	this.dragon.animations.add('fly', [0, 1, 2, 3], 12, true);
+    	this.dragon.animations.play('fly');
+    	this.dragon.anchor.set(0.5, 0.5);
+
+    	game.stage.backgroundColor="#26C9FF";
     },
 
     startGame: function() {
